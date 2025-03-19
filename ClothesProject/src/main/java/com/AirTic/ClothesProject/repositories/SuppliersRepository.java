67f005e41117a4a14b47d99a568956e3d0a8aa90
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-03-19 02:57:50 -0500

    Delete ClothesProject directory

--- ClothesProject/src/main/java/com/AirTic/ClothesProject/repositories/SuppliersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AirTic.ClothesProject.repositories;
-
-import com.AirTic.ClothesProject.models.Suppliers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SuppliersRepository extends JpaRepository<Suppliers, Long>{
-}
