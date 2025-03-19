7944f55c0aca1a0e32ccb9103d3f4b540ea05384
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-03-19 02:59:25 -0500

    Add files via upload

---
+++ ClothesProject/src/test/java/com/AirTic/ClothesProject/ClothesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AirTic.ClothesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
